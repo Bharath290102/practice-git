2730dbe80d3b7fb1c518da7efe0623f38d1bc9ed
Bharath290102 <bharathreddy6382@gmail.com>
2024-02-29 12:17:33 +0530

    Employees are Added

--- Employee1.java
+++ Employee1.java
@@ -17,7 +17,10 @@ public class Employee {
 public static void main(String[] args) {
 	Employee e1=new Employee(1,"Deva",99988989.909);
 	Employee e2=new Employee(2,"Salaar",19988989.909);
-	Employee e3=new Employee(3,"Saaho",29988989.909);
+	Employee e3=new Employee(3,"mahi",29987989.909);
+        Employee e4=new Employee(4,"raina",2954989.909);
+        Employee e5=new Employee(5,"jaddu",299889.909);
+          
 	
 	ArrayList al=new ArrayList();
 	al.add(e1);  al.add(e2); al.add(e3);
